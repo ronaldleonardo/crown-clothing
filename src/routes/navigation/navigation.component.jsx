8b7ee2e6e287c6a7fce0ba021ae4ed7bba06d2c1
ronaldleonardo <86000468+ronaldleonardo@users.noreply.github.com>
2023-01-22 10:25:46 +0700

    adding cart context and shopping icon as well as cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,7 +2,12 @@ import { Fragment, useContext} from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -10,7 +15,7 @@ import './navigation.styles.scss'
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
-
+	const { isCartOpen } = useContext(CartContext);
   return(
     <Fragment>
     	<div className = 'navigation'>
@@ -23,14 +28,17 @@ const Navigation = () => {
 					<Link className = 'nav-link' to ='/shop'>
 						SHOP
 					</Link>
+
 					{ currentUser ? (
 						<span className='nav-link' onClick={signOutUser}>SIGN OUT </span>
 						) : (
 							<Link className = 'nav-link' to ='/auth'>SIGN IN</Link>
 						)
 					}
+					<CartIcon/>
 	    	</div>
-
+				{ isCartOpen &&	 <CartDropdown />} 
+						{/*if this total thing is true (isCartOpen is true and CartIcon (all component == true by default) i will return the last thing you gave me (CartDropdown))*/}	   
       </div>
       <Outlet />
     </Fragment>
